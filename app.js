e3e648832d3cc90f3832ab141ec22617c221f122
Gabriela Villa Angeles <angeles101na@gmail.com>
2019-04-30 13:19:23 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: '192.0.0.127',
+  port: 8080,
+  user: 'user',
+  password: '',
+  database: '',
+  // connectionLimit: 10,
+  // connectTimeout: 10000,
+  // waitForConnections: true,
+  // queueLimit: 0,
+   insecureAuth: true
+});
+
+connection.connect(function (err) {
+  (err) ? console.log('Error en la conexion') : console.log('Conectado');
+});
+
+require('./routes/html-routes')(app, connection);
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
